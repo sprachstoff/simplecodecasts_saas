257e45fad1ccc0391e7eb15204ed4c21fbc84347
John Fanta <fantahaus172@gmail.comm>
2015-08-13 17:56:37 +0000

    Added schema rb file

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150813175209) do
+
+  create_table "contacts", force: true do |t|
+  end
 
 end
