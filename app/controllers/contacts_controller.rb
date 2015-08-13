8ea32b1b59fda24b151a774f379d6476c61ff33a
John Fanta <fantahaus172@gmail.comm>
2015-08-13 17:47:07 +0000

    Added contacts page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,19 +4,19 @@ class ContactsController < ApplicationController
   end
   
   
-   def create
-    @contact = Contact.new(contact_params)
-    if @contact.save
-      redirect_to new_contact_path, notice: "Message sent."
-    else
-      redirect_to new_contact_path, notice: "Error occured."
-    end
-
-
+  def create
+  @contact = Contact.new(contact_params)
+  if @contact.save
+    flash[:success] = 'Message sent.'
+    redirect_to new_contact_path
+  else
+    flash[:danger] = 'Error occured, message has not been sent.'
+    redirect_to new_contact_path
+  end
   
   private
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
     end
-    
+  end
 end
